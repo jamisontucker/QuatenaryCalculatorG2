b62c4a36195bb23a5d64e54667787692fc2bb594
Jamison Tucker <jamison.tucker@bsu.edu>
2024-09-03 16:43:07 -0400

    Add buttons 0 through 3

--- src/srcwork/UI.java
+++ src/srcwork/UI.java
@@ -6,16 +6,21 @@ public class UI {
     public UI(){
         JFrame frame = new JFrame();
 
-        JButton button = new JButton("Click");
-        JLabel label = new JLabel("1");
+
+        JButton button0 = new JButton("0");
+        JButton button1 = new JButton("1");
+        JButton button2 = new JButton("2");
+        JButton button3 = new JButton("3");
 
 
 
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0,1));
-        panel.add(button);
-        panel.add(label);
+        panel.setLayout(new GridLayout(0,4));
+        panel.add(button0);
+        panel.add(button1);
+        panel.add(button2);
+        panel.add(button3);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
