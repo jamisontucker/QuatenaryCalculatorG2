4961b62cf0254e9bf2e7538a556aea305f4a07a5
Jamison Tucker <jamison.tucker@bsu.edu>
2024-09-03 18:54:47 -0400

    Add operation buttons

--- src/srcwork/UI.java
+++ src/srcwork/UI.java
@@ -6,21 +6,31 @@ public class UI {
     public UI(){
         JFrame frame = new JFrame();
 
-
+        JLabel calcLabel = new JLabel("");
         JButton button0 = new JButton("0");
         JButton button1 = new JButton("1");
         JButton button2 = new JButton("2");
         JButton button3 = new JButton("3");
+        JButton buttonPlus = new JButton("+");
+        JButton buttonSub = new JButton("-");
+        JButton buttonMul = new JButton("x");
+        JButton buttonDiv = new JButton("/");
+
 
 
 
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0,4));
+        panel.add(calcLabel);
         panel.add(button0);
         panel.add(button1);
         panel.add(button2);
         panel.add(button3);
+        panel.add(buttonPlus);
+        panel.add(buttonSub);
+        panel.add(buttonMul);
+        panel.add(buttonDiv);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
